66ec6b13538e701ec9e670ebbb649355fad65af7
zhibek <zhibekt@gmail.com>
2019-12-26 23:40:02 +0100

    sauzen.csv

--- src/main/java/be/vdab/frida/controllers/SausController.java
+++ src/main/java/be/vdab/frida/controllers/SausController.java
@@ -1,6 +1,7 @@
 package be.vdab.frida.controllers;
 
 import be.vdab.frida.domain.Saus;
+import be.vdab.frida.services.SausService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,40 +16,43 @@ import java.util.stream.Collectors;
 @Controller
 @RequestMapping("sauzen")
 public class SausController {
-    private final Saus[] sauzen = {
+    private final char[]alfabet="abcdefghijklmnopqrstuvwxyz".toCharArray();
+    private final SausService sausService;
+    SausController(SausService sausService) {
+        this.sausService = sausService;
+    }
+    /*private final Saus[] sauzen = {
             new Saus(1, "cocktail", new String[]{"ketchup, mayonaise, room, azijn"}),
             new Saus(2, "mayonaise", new String[]{"ei, olie"}),
             new Saus(3, "mosterd", new String[]{"mosterdzaden, azijn, water"}),
             new Saus(4, "tartare", new String[]{"ei, sjalot, mayonaise"}),
-            new Saus(5, "vinaigrette", new String[]{"olie, azijn, mosterd"})};
-
+            new Saus(5, "vinaigrette", new String[]{"olie, azijn, mosterd"})};*/
 
     @GetMapping
     public ModelAndView sauzen(){
-        return new ModelAndView("sauzen", "sauzen", sauzen);
+        return new ModelAndView("sauzen", "sauzen", sausService.findAll());
     }
 
     @GetMapping("{nummer}")
     public ModelAndView saus(@PathVariable long nummer){
         ModelAndView modelAndView= new ModelAndView("saus");
-        Arrays.stream(sauzen).filter(saus-> saus.getNummer() == nummer).findFirst()
-                .ifPresent(saus -> modelAndView.addObject("saus", saus));
+        sausService.findById(nummer)
+                .ifPresent(saus -> modelAndView.addObject(saus));
         return modelAndView;
     }
 
-    private final char[]alfabet="abcdefghijklmnopqrstuvwxyz".toCharArray();
     @GetMapping("alfabet")
     public ModelAndView alfabet(){
         return new ModelAndView("sausAlfabet", "alfabet", alfabet);
     }
-    private List<Saus> sauzenDieBeginnenMet(char letter){
+    /*private List<Saus> sauzenDieBeginnenMet(char letter){
         return Arrays.stream(sauzen).filter(saus -> saus.getNaam().charAt(0)==letter)
-                .collect(Collectors.toList());
-    }
+                .collect(Collectors.toList());}*/
+
     @GetMapping("alfabet/{letter}")
     public ModelAndView sauzenBeginnendMet(@PathVariable char letter){
         return new ModelAndView("sausalfabet","alfabet", alfabet)
-                .addObject("sauzen", sauzenDieBeginnenMet(letter));
+                .addObject("sauzen", sausService.findByNaamBegintMet(letter));
     }
 
 }
---
+++ src/main/java/be/vdab/frida/services/SausService.java
@@ -0,0 +1,13 @@
+package be.vdab.frida.services;
+
+import be.vdab.frida.domain.Saus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SausService {
+    List<Saus> findAll();
+    List<Saus> findByNaamBegintMet(char letter);
+    Optional<Saus> findById(long id);
+}
